af2bdaf50b1a7d0b293a1d5d045c66e65508bb3d
kriddie <whitmore.kriddie@gmail.com>
2021-06-25 18:04:03 -0400

    Script for methane sampling

--- KriddieFolder/CH4_Script.R
+++ KriddieFolder/CH4_Script.R
@@ -6,16 +6,18 @@
 
 library(lubridate) #package for our date parsing
 library(dplyr)
+library(tidyverse)
 library(here)
+
 
 
 ##set folder for site ##
 
-setwd(here::here("Lux"))
+setwd(here::here("CH4"))
 
 all_files=list.files(pattern=".csv") #pulls out the csv files from WL folder in HOBOware folder
 sites_rp=gsub("_.*","",all_files) #selects the correct pattern so as to seelct only desired files
-sites_rp = sub('_[^_]+$', '', all_files)
+#sites_rp = sub('_[^_]+$', '', all_files)
 
 site_names=unique(sites_rp) #creates list of site names for following loop
 
@@ -32,26 +34,53 @@ for (site in site_names){
   #reads in files in list and appends
   for (file in file_list){
     if (!exists("EOSData")){
-      EOSData <- read.csv(file, skip=2, header = FALSE, sep = ",",
+      EOSData <- read.csv(file, skip=0, header = TRUE, sep = ",",
                          quote = "\"",dec = ".", fill = TRUE, comment.char = "")
-      
-    }
+      EOSData <- select(EOSData, c("Month", "Day", "Year", "Time","Flux","Temperature..C.","Mode","EOS.Num","Site","Trans.Num"))
+#      colnames(EOSData)=c("Month","Day","Year","Time","Flux","Temperature_c","Mode","Eos_no","Site","Trans_no")
+      }
     if (exists("EOSData")){
-      Temp_EOSData <- read.csv(file, skip=2, header = FALSE, sep = ",",
+      Temp_EOSData <- read.csv(file, skip=0, header = TRUE, sep = ",",
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")  
-      
+#      Temp_EOSData <- read_csv(file)
+#      Temp_EOSData[, c('Month', 'Day', 'Year', 'Time','Flux','`Temperature (C)`','`CO2 Soil (ppm)`','`CO2 Soil STD (ppm)`','`CO2 ATM (ppm)`','Mode','`EOS Num`','Site','`Trans Num`')]
+      Temp_EOSData <- select(Temp_EOSData, c("Month", "Day", "Year", "Time","Flux","Temperature..C.","Mode","EOS.Num","Site","Trans.Num"))
+ #     colnames(Temp_EOSData)=c("Month","Day","Year","Time","Flux","Temperature_c","Mode","Eos_no","Site","Trans_no")
       EOSData <- rbind(EOSData, Temp_EOSData)
       rm(Temp_EOSData)
     }
     
   }
-  colnames(EOSData)=c("row","DateTime","WLPres_kpa","WLTemp_c")
-  EOSData=EOSData[,2:4]
+  colnames(EOSData)=c("Month","Day","Year","Time","Flux","Temperature_c","Mode","Eos_no","Site","Trans_no")
+#  EOSData=EOSData[,1:14]
   EOSData=unique(EOSData)
-  EOSData$DateTime <- as.POSIXct(EOSData$DateTime, format="%m/%d/%y %I:%M:%S %p", tz="UTC")
-  assign((paste(site,"WL_data",sep="_")),EOSData) #creates object with new appended data
-  rm(EOSData) #removes WLdata so that multiple sites aren't appended together
+  EOSData$Date <- as.Date(with(EOSData, paste(EOSData$Year + 2000, EOSData$Month, EOSData$Day,sep="-")), "%Y-%m-%d")
+  EOSData$DateTime <- as.POSIXct(paste(EOSData$Date, EOSData$Time), format="%Y-%m-%d %H:%M:%S")
+  assign((paste(site,sep="_")),EOSData) #creates object with new appended data
+  rm(EOSData)
 }
 
+EOS <- EOS  %>%
+drop_na(Site)   
 
+EOS_pivot <- EOS  %>%
+group_by(Date, Site, Trans_no) %>%
+  filter(Flux > 0)  %>%
+  summarize(mean_Flux = mean(Flux, na.rm = TRUE),
+            std_Flux = sd(Flux, na.rm = TRUE))
+
+
+#Graph it up bitches
+
+ggplot(EOS_pivot, aes(fill=Site, y=mean_Flux, x=Trans_no)) + 
+  geom_bar(position="dodge", stat="identity")
+
+ggplot(EOS_pivot, aes(y=mean_Flux, x=Trans_no)) + 
+  geom_bar(position="dodge", stat="identity")
+
+EOS_pivot$percent <- EOS_pivot$std_Flux / EOS_pivot$mean_Flux *100
+
+
+ggplot(EOS_pivot, aes(fill=Site, y=percent, x=Trans_no)) + 
+  geom_bar(position="dodge", stat="identity")
 
