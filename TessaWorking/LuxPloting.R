bc7ad5d7ec497f744ea4df3a56690db0b742439e
tessamd11 <tessamd@live.unc.edu>
2021-07-02 16:12:00 -0500

    Working on script

--- TessaWorking/LuxPloting.R
+++ TessaWorking/LuxPloting.R
@@ -6,25 +6,34 @@ library(ggplot2)
 
 #plotting individual lux sensors
 ggplot(LUXabajo_01_Lux_data, aes(x=DateTime, y=Lux)) + 
-    geom_point(shape=1)
+    geom_point(shape=1)+
+  ggtitle("LUXabajo_01")
 
 ggplot(LUXabajo_02_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)
+  geom_point(shape=1)+
+  ggtitle("LUXabajo_02")
 
 ggplot(LUXabajo_03_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)
+  geom_point(shape=1)+
+  ggtitle("LUXabajo_02")
 
 ggplot(LUXabajo_04_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)
+  geom_point(shape=1)+
+  ggtitle("LUXabajo_04")
 
 ggplot(LUXarriba_01_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)  
+  geom_point(shape=1) +
+  ggtitle("LUXarriba_01") 
 
 ggplot(LUXarriba_02_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1) 
+  geom_point(shape=1) +
+  ggtitle("LUXarriba_02") 
 
 ggplot(LUXarriba_03_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)
+  geom_point(shape=1)+
+  ggtitle("LUXarriba_01") 
 
 ggplot(LUXarriba_04_Lux_data, aes(x=DateTime, y=Lux)) +
-  geom_point(shape=1)
+  geom_point(shape=1)+
+  ggtitle("LUXarriba_01") 
+
