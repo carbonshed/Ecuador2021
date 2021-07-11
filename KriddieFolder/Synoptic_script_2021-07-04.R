00332a228af1d61214bc1284bd2d278b9b6bae69
kriddie <whitmore.kriddie@gmail.com>
2021-07-11 16:41:04 -0400

    Edits to synoptic Data

--- KriddieFolder/Synoptic_script_2021-07-04.R
+++ KriddieFolder/Synoptic_script_2021-07-04.R
@@ -21,12 +21,14 @@ register_google(key = API_Key, write = TRUE)
 CO2_eos1_June18 <-  read.csv(here::here("/Synoptic/June18_Edited/CO2_EOS1_synoptic_EDITED_2021-06-18.csv"), skip=6, header = TRUE, sep = ",",
                             quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos1_June18) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample", "Notes", "Notes_2")
+CO2_eos1_June18$EOS_no <- "EOS_1"
 CO2_June18 <- CO2_eos1_June18
 
 #June 22
 CO2_eos1_June22 <-  read.csv(here::here("/Synoptic/June22/CO2_EOS1synoptic_2021-06-22.csv"), skip=6, header = TRUE, sep = ",",
                  quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos1_June22) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos1_June22$EOS_no <- "EOS_1"
 CO2_eos1_June22$Notes <- NA
 CO2_eos1_June22$Notes_2 <- NA
 
@@ -34,36 +36,40 @@ CO2_eos2_June22 <-  read.csv(here::here("/Synoptic/June22/CO2_EOS2synoptic_2021-
                  quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 CO2_eos2_June22$Voltage..ppm. <- NULL
 colnames(CO2_eos2_June22) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos2_June22$EOS_no <- "EOS_2"
 CO2_eos2_June22$Notes <- NA
 CO2_eos2_June22$Notes_2 <- NA
 
 CO2_June22 <- rbind(CO2_eos1_June22,CO2_eos2_June22)
 
 ## June  23
-CO2_eos1_June23 <-  read.csv(here::here("/Synoptic/June23/CO2_EOS1_Synoptics_2021-06-23.csv"), skip=6, header = TRUE, sep = ",",
+CO2_eos1_June23 <-  read.csv(here::here("/Synoptic/June23/CO2_EOS1_Synoptics_EDITAGAIN_2021-06-23.csv"), skip=6, header = TRUE, sep = ",",
                       quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 CO2_eos1_June23$Voltage..ppm. <- NULL
 colnames(CO2_eos1_June23) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos1_June23$EOS_no <- "EOS_1"
 CO2_eos1_June23$Notes <- NA
 CO2_eos1_June23$Notes_2 <- NA
 
 CO2_eos2_June23 <-  read.csv(here::here("/Synoptic/June23/CO2_EOS2_Synoptics_2021-06-23.csv"), skip=6, header = TRUE, sep = ",",
                              quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos2_June23) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos2_June23$EOS_no <- "EOS_2"
 CO2_eos2_June23$Notes <- NA
 CO2_eos2_June23$Notes_2 <- NA
 
 CO2_June23 <- rbind(CO2_eos1_June23,CO2_eos2_June23)
 
 ## June  29
-CO2_eos1_June29 <-  read.csv(here::here("/Synoptic/June29_Edited/EOS1_CO2_Edited_2021-06-29.csv"), skip=6, header = TRUE, sep = ",",
+CO2_eos1_June29 <-  read.csv(here::here("/Synoptic/June29_Edited/EOS1_CO2_Edited_2021-06-29_EditAgain.csv"), skip=6, header = TRUE, sep = ",",
                              quote = "\"",dec = ".", fill = TRUE, comment.char = "")
-colnames(CO2_eos1_June29) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample","Notes")
-CO2_eos1_June29$Notes_2 <- NA
+colnames(CO2_eos1_June29) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample","Notes","Notes_2")
+CO2_eos1_June29$EOS_no <- "EOS_1"
 
 CO2_eos2_June29 <-  read.csv(here::here("/Synoptic/June29_Edited/EOS2_CO2_Edited_2021-06-29.csv"), skip=6, header = TRUE, sep = ",",
                              quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos2_June29) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos2_June29$EOS_no <- "EOS_2"
 CO2_eos2_June29$Notes <- NA
 CO2_eos2_June29$Notes_2 <- NA
 
@@ -75,6 +81,7 @@ CO2_June29 <- rbind(CO2_eos1_June29,CO2_eos2_June29)
 CO2_eos1_June30 <-  read.csv(here::here("/Synoptic/June30_Edited/EOS1_CO2_Edited_2021-06-30.csv"), skip=6, header = TRUE, sep = ",",
                              quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos1_June30) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample","Notes")
+CO2_eos1_June30$EOS_no <- "EOS_1"
 CO2_eos1_June30$Notes_2 <- NA
 CO2_eos1_June30$Lon <- as.numeric(CO2_eos1_June30$Lon)
 CO2_eos1_June30$Lat <- as.numeric(CO2_eos1_June30$Lat)
@@ -82,6 +89,7 @@ CO2_eos1_June30$Lat <- as.numeric(CO2_eos1_June30$Lat)
 CO2_eos2_June30 <-  read.csv(here::here("/Synoptic/June30_Edited/EOS02_CO2_EDITED_2021-06-30.csv"), skip=6, header = TRUE, sep = ",",
                              quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 colnames(CO2_eos2_June30) <- c("Date","Time","CO2_ppm","Tract","Description","Point","Lon","Lat","WaterSample")
+CO2_eos2_June30$EOS_no <- "EOS_2"
 CO2_eos2_June30$Notes <- NA
 CO2_eos2_June30$Notes_2 <- NA
 
@@ -96,7 +104,7 @@ CO2_synop$Time <- NULL
 
 CO2_synop_pivot <- CO2_synop  %>%
   drop_na(Lat)  %>%
-  group_by(Lat, Lon, Date) %>%
+  group_by(Lat, Lon, Date, EOS_no) %>%
   summarize(CO2_ppm_ave = mean(CO2_ppm, na.rm = TRUE),
             Tract = mean(Tract, na.rm = TRUE),
             Point = mean(Point, na.rm = TRUE)) 
@@ -115,6 +123,7 @@ Flux_eos1_June18 <-  read.csv(here::here("/Synoptic/June18_Edited/EOS1_Edited_20
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos1_June18 <- Flux_eos1_June18[,c(1:6,12:17)]
 colnames(Flux_eos1_June18) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos1_June18$EOS_no <- "EOS_1"
 Flux_eos1_June18$Date <- as.Date(with(Flux_eos1_June18, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_June18 <- Flux_eos1_June18
@@ -125,12 +134,14 @@ Flux_eos1_June22 <-  read.csv(here::here("/Synoptic/June22/EOS1_Synoptic_2021-06
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos1_June22 <- Flux_eos1_June22[,c(1:6,12:17)]
 colnames(Flux_eos1_June22) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos1_June22$EOS_no <- "EOS_1"
 Flux_eos1_June22$Date <- as.Date(with(Flux_eos1_June22, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_eos2_June22 <-  read.csv(here::here("/Synoptic/June22/EOS2_Synoptic_2021-06-22_EditedAgain.csv"), skip=0, header = TRUE, sep = ",",
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos2_June22 <- Flux_eos2_June22[,c(1:6,16:21)]
 colnames(Flux_eos2_June22) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos2_June22$EOS_no <- "EOS_2"
 Flux_eos2_June22$Date <- as.Date(with(Flux_eos2_June22, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_June22 <- rbind(Flux_eos1_June22,Flux_eos2_June22)
@@ -140,12 +151,14 @@ Flux_eos1_June23 <-  read.csv(here::here("/Synoptic/June23/EOS1_Synoptics_2021-0
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos1_June23 <- Flux_eos1_June23[,c(1:6,12:17)]
 colnames(Flux_eos1_June23) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos1_June23$EOS_no <- "EOS_1"
 Flux_eos1_June23$Date <- as.Date(with(Flux_eos1_June23, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_eos2_June23 <-  read.csv(here::here("/Synoptic/June23/EOS2_Synoptics_2021-06-23_EditedAgain.csv"), skip=0, header = TRUE, sep = ",",
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos2_June23 <- Flux_eos2_June23[,c(1:6,16:21)]
 colnames(Flux_eos2_June23) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos2_June23$EOS_no <- "EOS_2"
 Flux_eos2_June23$Date <- as.Date(with(Flux_eos2_June23, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_June23 <- rbind(Flux_eos1_June23,Flux_eos2_June23)
@@ -155,12 +168,14 @@ Flux_eos1_June29 <-  read.csv(here::here("/Synoptic/June29_Edited/EOS1_Edited_20
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos1_June29 <- Flux_eos1_June29[,c(1:6,12:17)]
 colnames(Flux_eos1_June29) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos1_June29$EOS_no <- "EOS_1"
 Flux_eos1_June29$Date <- as.Date(with(Flux_eos1_June29, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_eos2_June29 <-  read.csv(here::here("/Synoptic/June29_Edited/EOS2_Edited_2021-06-29_EditedAgain.csv"), skip=0, header = TRUE, sep = ",",
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos2_June29 <- Flux_eos2_June29[,c(1:6,16:21)]
 colnames(Flux_eos2_June29) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos2_June29$EOS_no <- "EOS_2"
 Flux_eos2_June29$Date <- as.Date(with(Flux_eos2_June29, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_June29 <- rbind(Flux_eos1_June29,Flux_eos2_June29)
@@ -171,12 +186,14 @@ Flux_eos1_June30 <-  read.csv(here::here("/Synoptic/June30_Edited/EOS1_Edited_20
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos1_June30 <- Flux_eos1_June30[,c(1:6,12:17)]
 colnames(Flux_eos1_June30) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos1_June30$EOS_no <- "EOS_1"
 Flux_eos1_June30$Date <- as.Date(with(Flux_eos1_June30, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_eos2_June30 <-  read.csv(here::here("/Synoptic/June30_Edited/EOS2_Synoptic_EDITED_2021-06-30_EditedAgain.csv"), skip=0, header = TRUE, sep = ",",
                               quote = "\"",dec = ".", fill = TRUE, comment.char = "")
 Flux_eos2_June30 <- Flux_eos2_June30[,c(1:6,16:21)]
 colnames(Flux_eos2_June30) <- c("Month","Day","Year","Time","Flux","Temp","Tract","Description","Point","Lon","Lat","WaterSample")
+Flux_eos2_June30$EOS_no <- "EOS_2"
 Flux_eos2_June30$Date <- as.Date(with(Flux_eos2_June30, paste(Year, Month, Day,sep="-")), "%y-%m-%d")
 
 Flux_June30 <- rbind(Flux_eos1_June30,Flux_eos2_June30)
@@ -184,14 +201,14 @@ Flux_June30 <- rbind(Flux_eos1_June30,Flux_eos2_June30)
 
 Flux_June30_pivot2 <- Flux_eos2_June30  %>%
   drop_na(Lat)  %>%
-  group_by(Lat, Lon, Date) %>%
+  group_by(Lat, Lon, Date, EOS_no) %>%
   summarize(Flux_ave = mean(Flux, na.rm = TRUE),
             Tract = mean(Tract, na.rm = TRUE),
             Point = mean(Point, na.rm = TRUE)) 
 
 
-Flux_map <- qmplot(Lon, Lat, data = Flux_June30_pivot, zoom = 13,  maptype = "toner-background", color = Flux_ave)+
-  scale_color_gradient(low="blue", high="red")
+#Flux_map <- qmplot(Lon, Lat, data = Flux_June30_pivot, zoom = 13,  maptype = "toner-background", color = Flux_ave)+
+#  scale_color_gradient(low="blue", high="red")
 
 
 
@@ -199,7 +216,7 @@ Flux_synop <- rbind(Flux_June18, Flux_June22, Flux_June23, Flux_June29, Flux_Jun
 
 Flux_synop_pivot <- Flux_synop  %>%
   drop_na(Lat)  %>%
-  group_by(Lat, Lon, Date) %>%
+  group_by(Lat, Lon, Date, EOS_no) %>%
   summarize(Flux_ave = mean(Flux, na.rm = TRUE),
             Tract = mean(Tract, na.rm = TRUE),
             Point = mean(Point, na.rm = TRUE)) 
@@ -212,9 +229,9 @@ CO2_map <- qmplot(Lon, Lat, data = CO2_synop_pivot, zoom = 13,  maptype = "toner
 
 ### Merge data
 
-synop_merge <- full_join(Flux_synop_pivot,CO2_synop_pivot, by = c("Lat","Lon","Date", "Tract", "Point"))
+synop_merge <- full_join(Flux_synop_pivot,CO2_synop_pivot, by = c("Lat","Lon","Date", "Tract", "Point","EOS_no"))
 
-write.csv(synop_merge, here::here())
+write.csv(synop_merge, here::here("Synoptic/GAVI.csv"))
 
 synop_merge$Date.as.fact <- as.factor(synop_merge$Date)
 
