bd612fcb301d6fdba1a195108dfb3e36ba013981
Kriddie Whitmore <whitmore.kriddie@gmail.com>
2024-09-17 15:31:08 -0400

    working on spreadsheet to read into r

--- KriddieFolder/Synoptic_Script/interpolate_Q.R
+++ KriddieFolder/Synoptic_Script/interpolate_Q.R
@@ -34,7 +34,7 @@ Ante <- read.csv(here::here("ProcessedData/export_to_r/ante_XWD_export_eledata_c
 #Colm
 Colm$x_pred <- round_to_first_decimal(Colm$x_pred)
 Colm_join_df <- data.frame(x_pred=seq(from = min(Colm$x_pred), to = max(Colm$x_pred), by = .1))
-Colm_join_df <- left_join(Colm_join_df,Colm,by="x_pred")
+Colm_join_df <- full_join(Colm_join_df,Colm,by="x_pred")
 Colm_join_df <- Colm_join_df[order(Colm_join_df$x_pred),]
 
 Colm_join_df <- Colm_join_df %>%
@@ -43,14 +43,15 @@ Colm_join_df <- Colm_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 
 #Ante
 Ante$x_pred <- round_to_first_decimal(Ante$x_pred)
 Ante_join_df <- data.frame(x_pred=seq(from = min(Ante$x_pred), to = max(Ante$x_pred), by = .1))
-Ante_join_df <- left_join(Ante_join_df,Ante,by="x_pred")
+Ante_join_df <- full_join(Ante_join_df,Ante,by="x_pred")
 Ante_join_df <- Ante_join_df[order(Ante_join_df$x_pred),]
 
 Ante_join_df <- Ante_join_df %>%
@@ -59,13 +60,14 @@ Ante_join_df <- Ante_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 #GaviDown
 GaviDown$x_pred <- round_to_first_decimal(GaviDown$x_pred)
 GaviDown_join_df <- data.frame(x_pred=seq(from = min(GaviDown$x_pred), to = max(GaviDown$x_pred), by = .1))
-GaviDown_join_df <- left_join(GaviDown_join_df,GaviDown,by="x_pred")
+GaviDown_join_df <- full_join(GaviDown_join_df,GaviDown,by="x_pred")
 GaviDown_join_df <- GaviDown_join_df[order(GaviDown_join_df$x_pred),]
 
 GaviDown_join_df <- GaviDown_join_df %>%
@@ -74,13 +76,14 @@ GaviDown_join_df <- GaviDown_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 #gaviup
 GaviUp$x_pred <- round_to_first_decimal(GaviUp$x_pred)
 GaviUp_join_df <- data.frame(x_pred=seq(from = min(GaviUp$x_pred), to = max(GaviUp$x_pred), by = .1))
-GaviUp_join_df <- left_join(GaviUp_join_df,GaviUp,by="x_pred")
+GaviUp_join_df <- full_join(GaviUp_join_df,GaviUp,by="x_pred")
 GaviUp_join_df <- GaviUp_join_df[order(GaviUp_join_df$x_pred),]
 
 GaviUp_join_df <- GaviUp_join_df %>%
@@ -89,14 +92,15 @@ GaviUp_join_df <- GaviUp_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 
 #gavi trib 1
 GaviTrib1$x_pred <- round_to_first_decimal(GaviTrib1$x_pred)
 GaviTrib1_join_df <- data.frame(x_pred=seq(from = min(GaviTrib1$x_pred,na.rm = TRUE), to = max(GaviTrib1$x_pred,na.rm = TRUE), by = .1))
-GaviTrib1_join_df <- left_join(GaviTrib1_join_df,GaviTrib1,by="x_pred")
+GaviTrib1_join_df <- full_join(GaviTrib1_join_df,GaviTrib1,by="x_pred")
 GaviTrib1_join_df <- GaviTrib1_join_df[order(GaviTrib1_join_df$x_pred),]
 
 GaviTrib1_join_df <- GaviTrib1_join_df %>%
@@ -105,13 +109,14 @@ GaviTrib1_join_df <- GaviTrib1_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 #gavi trib 2
 GaviTrib2$x_pred <- round_to_first_decimal(GaviTrib2$x_pred)
 GaviTrib2_join_df <- data.frame(x_pred=seq(from = min(GaviTrib2$x_pred), to = max(GaviTrib2$x_pred), by = .1))
-GaviTrib2_join_df <- left_join(GaviTrib2_join_df,GaviTrib2,by="x_pred")
+GaviTrib2_join_df <- full_join(GaviTrib2_join_df,GaviTrib2,by="x_pred")
 GaviTrib2_join_df <- GaviTrib2_join_df[order(GaviTrib2_join_df$x_pred),]
 
 GaviTrib2_join_df <- GaviTrib2_join_df %>%
@@ -120,7 +125,8 @@ GaviTrib2_join_df <- GaviTrib2_join_df %>%
   mutate(Flux_ave = na.approx(Flux_ave))%>%
   mutate(WaterTemp_c = na.approx(WaterTemp_c))%>%
   mutate(Total_hPa = na.approx(Total_hPa))%>%
-  mutate(AirTemp_c = na.approx(AirTemp_c))
+  mutate(AirTemp_c = na.approx(AirTemp_c))%>%
+  mutate(catchment_ha = na.approx(catchment_ha))
 
 
 #add name column
@@ -133,14 +139,14 @@ Colm_join_df$name <- "Colm"
 Ante_join_df$name <- "Ante"
 
 #clean up dataframes
-GaviDown_join_df <- GaviDown_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
-GaviUp_join_df <- GaviUp_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)%>%rename(date=Date)
-GaviTrib1_join_df <- GaviTrib1_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)%>%rename(date=Date)
-GaviTrib2_join_df <- GaviTrib2_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,Flux_ave,adjusted_ppm,WaterTemp_c,Q_m3s,AirTemp_c,Total_hPa)%>%rename(date=Date)
+GaviDown_join_df <- GaviDown_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
+GaviUp_join_df <- GaviUp_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)%>%rename(date=Date)
+GaviTrib1_join_df <- GaviTrib1_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)%>%rename(date=Date)
+GaviTrib2_join_df <- GaviTrib2_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,Date,VaisalaType,EOS_no,Flux_ave,adjusted_ppm,WaterTemp_c,Q_m3s,AirTemp_c,Total_hPa)%>%rename(date=Date)
 GaviTrib2_join_df$DOC <- NA 
 GaviTrib2_join_df$TDN <- NA
-Colm_join_df <- Colm_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
-Ante_join_df <- Ante_join_df%>%select(name,notes,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
+Colm_join_df <- Colm_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
+Ante_join_df <- Ante_join_df%>%select(name,notes,catchment_ha,lat_fit,lon_fit,x_pred,ele_fit,ele_arcpro,dist_diff,slope_mid,slope_up,x,d,w,date,VaisalaType,EOS_no,DOC,TDN,Flux_ave,adjusted_ppm,Q_m3s,WaterTemp_c,AirTemp_c,Total_hPa)
 
 #rm(Ante,Colm,GaviDown,GaviUp,GaviTrib1,GaviTrib2)
 
@@ -207,12 +213,15 @@ allsites_df$k600_eq1_dummySlope <- (allsites_df$v_ms * .01)^.89 * (allsites_df$d
 #eD = gSV ; where g is acceleration due to gravity = 9.81 m/s/s ; S is slope ; V is velocity (m/s)) (units m2 s-3) - Raymond et al. 2012
 #eD break point is 0.020 m2 s–3
 allsites_df$eD <- 9.81*allsites_df$slope_mid*allsites_df$v_ms
-allsites_df$k600_eq1_ulsethcorrection <- exp(5.137 + log(.468*allsites_df$v_ms * allsites_df$slope_mid) *.242*log(allsites_df$d))
+#allsites_df$k600_eq1_ulsethcorrection <- allsites_df$k600_eq1 * 1.58 - .54
+allsites_df$k600_eq1_ulsethcorrection <- -0.54 + 1.58*
+  exp(5.137 + .468*log(allsites_df$v_ms * allsites_df$slope_mid) + .242*log(allsites_df$d))
 
 allsites_df$k600_eq1_final <- NA
+allsites_df <- allsites_df%>%drop_na(eD)
 
 for(i in 1:nrow(allsites_df)) {
-  if (allsites_df$eD < .020 ){
+  if (allsites_df$eD[i] < .020 ){
     allsites_df$k600_eq1_final[i] <- allsites_df$k600_eq1[i]
   }else{
     allsites_df$k600_eq1_final[i] <- allsites_df$k600_eq1_ulsethcorrection[i]
@@ -234,12 +243,12 @@ gavidown <- allsites_df%>%filter(name=="GaviDown")%>%drop_na(F_CO2_molpers_eq1)
 gavitrib1 <- allsites_df%>%filter(name=="GaviTrib1")%>%drop_na(F_CO2_molpers_eq1)
 gavitrib2 <- allsites_df%>%filter(name=="GaviTrib2")%>%drop_na(F_CO2_molpers_eq1)
 
-colm <- colm[order(colm$x_pred),]
-ante <- ante[order(ante$x_pred),]
-gaviup <- gaviup[order(gaviup$x_pred),]
-gavidown <- gavidown[order(gavidown$x_pred),]
-gavitrib1 <- gavitrib1[order(gavitrib1$x_pred),]
-gavitrib2 <- gavitrib2[order(gavitrib2$x_pred),]
+colm <- colm[order(colm$x),]
+ante <- ante[order(ante$x),]
+gaviup <- gaviup[order(gaviup$x),]
+gavidown <- gavidown[order(gavidown$x),]
+gavitrib1 <- gavitrib1[order(gavitrib1$x),]
+gavitrib2 <- gavitrib2[order(gavitrib2$x),]
 
 colm$flux_sum <- NA
 ante$flux_sum <- NA
@@ -300,81 +309,11 @@ for(i in 1:nrow(gavitrib2)) {
   }
 }
 
+allsites_df <- rbind(colm,ante,gaviup,gavidown,gavitrib1,gavitrib2)
+
 #write out
-#write.csv(allsites_df, here::here("ProcessedData/raymond_data_allsites_2024-09-13.csv"))
-
-#plot
-
-allsites_df_plot <- allsites_df%>%drop_na(EOS_no)
-allsites_df_plot <- allsites_df_plot%>%drop_na(k600_eq1)
-
-ggplot(allsites_df, aes(x=Flux_mol_m2_d,F_mol_m2_d_Raymond,color= slope)) + geom_point() + scale_y_log10() + scale_x_log10()
-ggplot(allsites_df, aes(x=w,F_mol_m2_d_Raymond,color= name)) + geom_point()
-ggplot(allsites_df, aes(x=slope,F_mol_m2_d_Raymond,color= name)) + geom_point()
-
-ggplot(allsites_df, aes(x=slope,adjusted_ppm,color= name)) + geom_point() +
-  scale_y_log10() + scale_x_log10()
+#write.csv(allsites_df, here::here("ProcessedData/raymond_data_allsites_2024-09-16.csv"))
 
 
-#what is the most important factor in flux?
-#is it slope? width? velocity?
 
-ggplot(allsites_df%>%filter(name=="GaviDown"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-ggplot(allsites_df%>%filter(name=="GaviDown"), aes(x=adjusted_ppm,F_CO2,color=slope)) + geom_point()
-
-ggplot(allsites_df%>%filter(name=="Colm"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-ggplot(allsites_df%>%filter(name=="Ante"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-ggplot(allsites_df%>%filter(name=="GaviUp"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-ggplot(allsites_df%>%filter(name=="GaviTrib1"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-ggplot(allsites_df%>%filter(name=="GaviTrib2"), aes(x=adjusted_ppm,F_CO2)) + geom_point()
-
-
-ggplot(allsites_df%>%filter(name!="Colm"), aes(x=slope,F_CO2,color=name)) + 
-  geom_point() + scale_y_log10()
-ggplot(allsites_df%>%filter(name=="GaviUp")
-       , aes(x=slope,F_CO2,color=name)) + 
-  geom_point() + scale_y_log10() + scale_x_log10()+ 
-  geom_smooth(method='lm', formula= y~x)
-
-
-ggplot(allsites_df, aes(x=slope,k600_eq1)) +
-  geom_point()
-ggplot(allsites_df_plot, aes(x=k600_chamber,y=k600_eq1_dummySlope,color=name)) +
-  geom_point() +
-  geom_abline(intercept = 0, slope = 1)
-
-
-ggplot(allsites_df
-       %>%filter(name!="Colm")%>%filter(name!="GaviDown"),
-       aes(x=slope,log(adjusted_ppm),color=name)) + geom_point()+ 
-  geom_smooth(method='lm', formula= y~x)
-
-ggplot(allsites_df, aes(x=x,slope,color=x)) + geom_point()
-
-#bball plots
-ggplot(allsites_df_plot,aes(x=x_pred,y=F_CO2_umolpers,color=name)) + geom_line()
-
-#adjust to ulseth
-
-#calc flux
-GaviDown_join_df$k_co2 <- 
-GaviDown_join_df$flux_eq1 <- GaviDown_join_df$adjusted_ppm * GaviDown_join_df$k600_eq1
-
-ggplot(allsites_df%>%filter(name=="GaviDown")
-       , aes(x=slope,F_CO2_umolpers,color=name)) + 
-  geom_point() + scale_y_log10() + scale_x_log10()
-
-ggplot(allsites_df%>%filter(name=="Colm")
-       , aes(x=slope,Flux_ave,color=name)) + 
-  geom_point() + scale_y_log10() + scale_x_log10()
-
-#################
-#plots of worth?#
-#################
-ggplot(allsites_df %>%filter(name=="Colm")
-       , aes(x=slope,F_CO2_umolpers,color=name)) + 
-  geom_point() + scale_y_log10() + scale_x_log10()
-#take-away: slope is a really strong predictor of flux
-ggplot(allsites_df %>%filter(name=="Colm")
-       , aes(x=slope,F_CO2_umolpers,color=name)) + 
-  geom_point() + scale_y_log10() + scale_x_log10()
+#Now, I want my CO2 data associated with the slopes. I think I'll just 
---
+++ RScripts/scratch/playwithdata.R
@@ -0,0 +1,140 @@
+library(BAMMtools)
+
+df1<- read_csv(here::here("ProcessedData/ALLSYNOPDATA_FINAL_2024-05-07.csv"))%>%rename(adjusted_ppm=pCO2_ppm)
+ggplot(df1%>%filter(Wetland=="ANTE"),aes(x=dist,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10()
+ggplot(df1%>%filter(Wetland=="GAVItrib1"),aes(x=dist,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10()
+ggplot(df1%>%filter(Wetland=="GAVItrib1"|Wetland=="GAVItrib3"|Wetland=="ANTE"),aes(x=CatchmentSize_ha,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10() +scale_x_log10()
+ggplot(df1#%>%filter(Wetland=="GAVItrib1"|Wetland=="GAVItrib3"|Wetland=="ANTE")
+       ,aes(x=CatchmentSize_ha,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10() +scale_x_log10()
+
+df <-read.csv(here::here("ProcessedData/raymond_data_allsites_2024-09-16.csv"),na.strings=c("","NA"))
+df$name_2 <- df$name
+df$name_2[which(df$name_2 == "GaviTrib1")] = "GaviTribs"
+df$name_2[which(df$name_2 == "GaviTrib2")] = "GaviTribs"
+
+df$water_subtract_air <- df$pCO2_w_atm - df$pCO2_air_atm
+
+
+getJenksBreaks(df$adjusted_ppm, 5, subset = NULL)
+# 320.1599  1499.8411  3042.7583  5341.6114 10343.6565
+
+ggplot(df%>%filter(name=="Ante"),aes(x=x_pred,y=ele_arcpro,color=log(adjusted_ppm))) + geom_point() + scale_color_gradient(low="blue", high="red")
+ggplot(df%>%filter(name=="GaviTrib1"),aes(x=x_pred,y=ele_arcpro,color=log(adjusted_ppm))) + geom_point() + scale_color_gradient(low="blue", high="red")
+ggplot(df%>%filter(name=="GaviTrib2"),aes(x=x_pred,y=ele_arcpro,color=log(adjusted_ppm))) + geom_point() + scale_color_gradient(low="blue", high="red")
+ggplot(df%>%filter(name=="GaviUp"),aes(x=x_pred,y=ele_arcpro,color=log(adjusted_ppm))) + geom_point() + scale_color_gradient(low="blue", high="red")
+ggplot(df%>%filter(name=="GaviDown"),aes(x=x_pred,y=ele_arcpro,color=log(adjusted_ppm))) + geom_point() + scale_color_gradient(low="blue", high="red")
+
+
+ggplot(df%>%filter(adjusted_ppm <= 320.1599),aes(x=adjusted_ppm,y=slope_up,color=name)) + geom_point()
+
+ggplot(df%>%filter(catchment_ha>2.5),aes(x=slope_up,y=adjusted_ppm,color=log(catchment_ha))) + geom_point() + scale_y_log10()
+
+
+
+CO2 <- ggplot(df, aes(x = name_2, y = adjusted_ppm, fill = name_2)) +
+  geom_boxplot() +
+  geom_jitter(shape=18,position=position_jitter(0.1)) +
+  stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, color = "black",fill="white")+
+  geom_hline(yintercept= log10(air_ppm),color="red",linetype="twodash", linewidth=1) +
+  scale_y_log10() +
+  MyTheme
+
+
+
+ggplot(df_bins,aes(x=Catchment_bin,y=w,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=d,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=slope_mid,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=Q_m3s,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=k600_eq1_final,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=adjusted_ppm,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=F_mol_m2_d_eq1,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=F_CO2_molpers_eq1,color=name)) + geom_point()+ scale_y_log10()
+
+
+ggplot(df_bins,aes(x=catchment_ha,y=w,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=d,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=slope_mid,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=Q_m3s,color=name)) + geom_point() + scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=k600_eq1_final,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=adjusted_ppm,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=F_mol_m2_d_eq1,color=name)) + geom_point()+ scale_y_log10()
+ggplot(df_bins,aes(x=catchment_ha,y=F_CO2_molpers_eq1,color=name)) + geom_point()+ scale_y_log10()
+
+
+
+ggplot(df_bins%>%filter(catchment_ha > 2.5)
+       %>%filter(slope_up > .05),
+       aes(x=slope_up,y=adjusted_ppm)) + 
+  geom_point()+ scale_y_log10() + geom_smooth(method='lm')
+
+
+ggplot(df_bins#%>%filter(catchment_ha > 2.5)
+       #%>%filter(slope_up > .001)
+       ,
+       aes(x=slope_mid,y=k600_eq1_final,color=Catchment_bin )) + 
+  geom_point()+ scale_y_log10() + geom_smooth(method='lm')
+
+
+#the first ___ of a stream is worth ___ flux
+
+
+
+df_bins_summary <-df_bins%>%
+  group_by(Catchment_bin) %>%
+  summarise(
+    k600_mean = mean(K600, na.rm = TRUE),
+    k600_stdev = sd(K600, na.rm = TRUE),
+    k600_median = median(K600, na.rm = TRUE),
+    k600_max = max(K600, na.rm = TRUE),
+    k600_min = min(K600, na.rm = TRUE),
+    n())
+
+df_bins$Catchment_bin2 <- as.character(df_bins$Catchment_bin)
+df_bins$Catchment_bin3 <- gsub("]", "", df_bins$Catchment_bin2) 
+df_bins$Catchment_bin4 <- sub('.', '', df_bins$Catchment_bin3)
+df_bins[c('min_log', 'max_log')] <- str_split_fixed(df_bins$Catchment_bin4, ',', 2)
+df_bins$min_log <- as.numeric(df_bins$min_log )
+df_bins$max_log <- as.numeric(df_bins$max_log )
+
+df_bins$min <- signif(exp(df_bins$min_log),digits =2)
+df_bins$max <- signif(exp(df_bins$max_log),digits =2)
+
+df_bins$bins_true  <- paste(df_bins$min, "-", df_bins$max)
+
+df_bins$bins_true = factor(df_bins$bins_true, levels=c("0.11 - 0.5","0.5 - 2.3","2.3 - 10","10 - 47","47 - 210"))
+
+
+### I am trying to come up with some things showing that really small streams act differenct from other streams
+
+ggplot(df%>%filter(catchment_ha>0),aes(x=slope_up,y=adjusted_ppm,color=log(catchment_ha))) + geom_point() + scale_y_log10()
+ggplot(df%>%filter(catchment_ha>2.5),aes(x=slope_up,y=adjusted_ppm,color=log(catchment_ha))) + geom_point() + scale_y_log10()
+ggplot(df%>%filter(catchment_ha>12.5),aes(x=slope_up,y=adjusted_ppm,color=log(catchment_ha))) + geom_point() + scale_y_log10()
+ggplot(df%>%filter(catchment_ha>20),aes(x=slope_up,y=adjusted_ppm,color=log(catchment_ha))) + geom_point() + scale_y_log10()
+
+ggplot(df_bins,aes(x=slope_mid,y=adjusted_ppm,color=name)) + geom_point() #+ scale_y_log10()
+
+ggplot(df_bins,aes(x=Catchment_bin,y=adjusted_ppm)) + geom_boxplot() + scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=F_mol_m2_d_eq1)) + geom_boxplot() + scale_y_log10()
+ggplot(df_bins,aes(x=Catchment_bin,y=F_CO2_molpers_eq1)) + geom_boxplot() + scale_y_log10()
+
+
+df1<- read_csv(here::here("ProcessedData/ALLSYNOPDATA_FINAL_2024-05-07.csv"))%>%rename(adjusted_ppm=pCO2_ppm)
+ggplot(df1%>%filter(Wetland=="GAVItrib1"|Wetland=="GAVItrib3"|Wetland=="ANTE"),aes(x=CatchmentSize_ha,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10() +scale_x_log10()
+ggplot(df1#%>%filter(Wetland=="GAVItrib1"|Wetland=="GAVItrib3"|Wetland=="ANTE")
+       ,aes(x=CatchmentSize_ha,y=adjusted_ppm,color=Wetland)) + 
+  geom_point() + scale_y_log10() +scale_x_log10()
+
+#take-away: slope is a really strong predictor of flux for COlM
+ggplot(df %>%filter(name=="Colm")
+       , aes(x=slope_mid,F_mol_m2_d_eq1,color=adjusted_ppm)) + 
+  geom_point() + scale_y_log10() + scale_x_log10()
+
+ggplot(df %>%filter(name=="Colm")
+       , aes(x=slope_mid,F_CO2_molpers_eq1,color=adjusted_ppm)) + 
+  geom_point() + scale_y_log10() + scale_x_log10()
+
